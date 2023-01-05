9b8e34ed0578133d89ed683f9aa87cd47924ff7d
Dmytro Komarov <dmitry.komarov90@gmail.com>
2023-01-05 23:37:14 +0200

    JS work with  let,constant,var

--- myfirstproject/js/script.js
+++ myfirstproject/js/script.js
@@ -1,23 +1,148 @@
-console.log('Учим JS');
-['Учим', 'JS'].forEach(alert);
 
-console.log('Учим' + ' JS');
+let user = 'Вася' let age = 36
 
-let num = 4;
-console.log(num);
-'Учим' + 'JS';
+let myage = 36;
 
-let num = 'Учим JS'
-console.log(num);
+const BLOCK_SIZE = 14 + 50;
 
-function name() {
+const BLOCK_HEIGHT = 100;
+
+if (true) {
+	var size = 15;
+}
+console.log(size);
+
+let age;
+let info123;
+let $size;
+let _color;
+
+let size;
+let age;
+let leftSidebarSize;
+
+let sidebarsize;
+
+let myLifeStyle;
+
+myLifeStyle = 'freelance';
+
+let myLifeStyle = 'freelance';
+
+console.log(myLifeStyle);
+
+let myName = 'Фрилансер по жизни', myAge = 36, myMessage = 'Живи, а работай в свободное время!';
+
+let myName = 'Фрилансер по жизни',
+	myAge = 36,
+	myMessage = 'Живи, а работай в свободное время!';
+
+let myName = 'Фрилансер по жизни';
+let myAge = 36;
+let myMessage = 'Живи, а работай в свободное время!';
+
+let myAge = 36;
+myAge = 18;
+console.log(myAge);
+
+
+let myAge = 36;
+let myNewAge = 18;
+
+
+myAge = myNewAge;
+console.log(myAge);
+
+myAge = 36;
+console.log(myAge);
+
+"use strict"
+
+let myAge = 36;
+console.log(myAge);
+
+
+function testBlock() {
 	console.log('Учим');
 	console.log('JS');
 }
-name();
+testBlock();
 
- "use strict";
+function testBlock() {
+	let myAge = 36;
+	console.log(myAge);
+}
+testBlock();
 
-console.log('Учим JS');
-"use strict";
+console.log(myAge);
 
+let myAge = 36;
+
+function testBlock() {
+	let myAge = 18;
+	console.log(myAge);
+}
+testBlock();
+
+console.log(myAge);
+
+let myAge = 18;
+
+for (let i = 0; i < 3; i++) {
+	console.log(i);
+
+}
+for (let i = 0; i < 3; i++) {
+	console.log(i);
+}
+
+const BLOCK_SIZE = 25;
+const COLOR_RED = "#F00";
+
+
+const summSizes = 25 + 30;
+
+const userProfile = {
+	name: 'Фрилансер',
+	age: 36,
+	message: 'Живи, а работай в свободное время!'
+}
+console.log(userProfile);
+
+userProfile.age = 18;
+console.log(userProfile);
+
+
+function testBlockLet() {
+	let myAgeLet = 36;
+}
+testBlockLet();
+
+function testBlockVar() {
+	var myAgeVar = 36;
+}
+testBlockVar();
+
+console.log(myAgeVar);
+*/
+
+/*
+if (true) {
+	let sizeLet = 50;
+}
+
+if (true) {
+	var sizeVar = 50;
+}
+
+console.log(sizeVar);
+
+let userName = 'Фрилансер по жизни';
+
+userName = 36;
+
+userName = 'Живи, а работай в свободное время!';
+
+let userName = 'Фрилансер по жизни';
+let userAge = 36;
+let userMassage = 'Живи, а работай в свободное время!';
