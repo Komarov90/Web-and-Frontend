3efc5a6af5ee10c85566041f736b5c93ea2e42d7
Dmytro Komarov <dmitry.komarov90@gmail.com>
2023-01-05 23:03:02 +0200

    Work with JS syntax

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Hello world!');
+console.log('Учим JS');
+['Учим', 'JS'].forEach(alert);
+
+console.log('Учим' + ' JS');
+
+let num = 4;
+console.log(num);
+'Учим' + 'JS';
+
+let num = 'Учим JS'
+console.log(num);
+
+function name() {
+	console.log('Учим');
+	console.log('JS');
+}
+name();
+
+ "use strict";
+
+console.log('Учим JS');
+"use strict";
+
