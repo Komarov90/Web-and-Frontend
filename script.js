66a2480fb7f5f8ded3cae3a804cdece06c8eabab
Dmytro Komarov <dmitry.komarov90@gmail.com>
2023-01-06 21:31:43 +0200

    JS function

--- script.js
+++ script.js
@@ -1,94 +1,324 @@
-let num = 0;
-while (num < 5) {
-	console.log(num);
-	num++;
+function showMessage() {
+	console.log('Сообщение');
 }
 
 
-let num = 5;
-while (num) {
-	console.log(num);
-	num--;
+function getSumm() {
+	let numOne, numTwo;
+
+	function getNumOne() {
+		numOne = 1;
+	}
+	function getNumTwo() {
+		numTwo = 2;
+	}
+	getNumOne();
+	getNumTwo();
+
+	let numSumm = numOne + numTwo;
+	console.log(numSumm);
 }
+getNumOne();
+getSumm();
 
 
-let num = 5;
-while (num) console.log(num--);
+"use strict"
+if (2 > 1) {
+	function getSumm() {
+		let numOne, numTwo;
 
-
-let num = 0;
-do {
-	console.log(num);
-	num++;
-} while (num < 0);
-
-for (Начало; Условие; Шаг) {
-
-}
-
-
-for (let num = 0; num < 5; num++) {
-	console.log(num);
-}
-
-for (let num = 0; num < 5; num++) {
-	console.log(num);
-}
-console.log(num);
-
-
-let num;
-for (num = 0; num < 5; num++) {
-	console.log(num);
-}
-console.log(`Вывод вне цикла: ${num}`);
-
-
-let num = 0;
-for (; num < 5; num++) {
-	console.log(num);
-}
-
-
-let num = 0;
-for (; num < 5;) {
-	num++;
-	console.log(num);
-}
-
-let num = 0;
-for (;;) {
-	num++;
-	console.log(num);
-}
-
-
-let num = 0;
-for (; num < 5; num++) {
-	console.log(num);
-	if (num == 2) break;
-}
-console.log(`Работа окончена, num = ${num}`);
-
-
-let num = 0;
-for (; num < 5; num++) {
-	if (num == 2) continue;
-	console.log(num);
-}
-
-
-let num = 0;
-for (; num < 5; num++) {
-	(num != 2) ? console.log(num) : continue;
-}
-
-
-firstFor: for (let num = 0; num < 2; num++) {
-	for (let size = 0; size < 3; size++) {
-		if (size == 2) {
-			continue firstFor;
+		function getNumOne() {
+			numOne = 1;
 		}
-		console.log(size);
+		function getNumTwo() {
+			numTwo = 2;
+		}
+		getNumOne();
+		getNumTwo();
+
+		let numSumm = numOne + numTwo;
+		console.log(numSumm);
 	}
 }
+getSumm();
+
+
+function showMessage() {
+	let message = "Сообщение";
+	console.log(message);
+}
+
+console.log(message);
+
+
+let message;
+
+function showMessage() {
+	message = "Сообщение";
+}
+console.log(message);
+
+showMessage();
+
+
+let message = "Сообщение №1";
+
+function showMessage() {
+	let message = "Сообщение №2";
+	console.log(message);
+}
+console.log(message);
+showMessage();
+
+
+let globalVar = "Я глобальная переменная";
+
+function getSumm() {
+	let numOne, numTwo;
+
+	function getNumOne() {
+		numOne = 1;
+		console.log(globalVar);
+	}
+	function getNumTwo() {
+		numTwo = 2;
+	}
+	getNumOne();
+	getNumTwo();
+
+	let numSumm = numOne + numTwo;
+	console.log(globalVar);
+}
+console.log(globalVar);
+
+getSumm();
+
+
+function calcSumm(numOne = 1, numTwo = 2) {
+	console.log(`Переменная numOne:${numOne}`);
+	console.log(`Переменная numTwo:${numTwo}`);
+
+	let numSumm = numOne + numTwo;
+
+	console.log(`Сумма:${numSumm}`);
+}
+
+calcSumm(5, 5);
+
+
+function calcSumm(numOne, numTwo, more, less) {
+	let numSumm = numOne + numTwo;
+
+	if (numSumm > 3) {
+		more();
+	} else {
+		less();
+	}
+}
+
+function showMoreMessage() {
+	console.log('Больше чем 3');
+}
+function showLessMessage() {
+	console.log('Меньше чем 3');
+}
+calcSumm(1, 1, showMoreMessage, showLessMessage);
+
+
+function calcSumm(numOne, numTwo) {
+
+	let numSumm = numOne + numTwo;
+	return numSumm;
+
+}
+let funcRezult = calcSumm(1, 2);
+
+console.log(`Сумма: ${funcRezult}`);
+
+
+function calcSumm(numOne, numTwo) {
+
+	let numSumm = numOne + numTwo;
+
+	if (numSumm >= 3) {
+		return numSumm;
+	} else {
+		return 'Сумма меньше 3';
+	}
+
+}
+let funcRezult = calcSumm(1, 2);
+
+console.log(funcRezult);
+
+
+function calcSumm(numOne, numTwo) {
+
+	let numSumm = numOne + numTwo;
+
+	return
+	numSumm;
+
+}
+let funcRezult = calcSumm(1, 2);
+
+console.log(funcRezult);
+
+
+function calcSumm(numOne, numTwo) {
+
+	let numSumm = numOne + numTwo;
+
+	if (numSumm >= 3) {
+		return;
+	}
+}
+calcSumm(5, 5)
+
+
+function getSumm(numOne, numTwo) {
+	let numSumm = calcSumm(numOne, numTwo);
+
+	console.log(numSumm);
+}
+function calcSumm(numOne, numTwo) {
+	return numOne + numTwo;
+
+}
+getSumm(5, 5);
+
+
+function calcSumm(numOne, numTwo) {
+	let result = 1;
+	for (let i = 0; i < numTwo; i++) {
+		result *= numOne;
+	}
+	return result;
+}
+console.log(calcSumm(2, 3));
+
+
+function calcSumm(numOne, numTwo) {
+	if (numTwo === 1) {
+		return numOne;
+	} else {
+		return numOne * calcSumm(numOne, numTwo - 1);
+	}
+}
+console.log(calcSumm(2, 3));
+
+
+let showMessage = function () {
+	console.log('Привет!');
+};
+
+showMessage();
+
+
+function getSumm() {
+	let summ = 1 + 2;
+	console.log(summ);
+};
+
+let someVar = getSumm;
+
+someVar();
+getSumm();
+
+
+function getSumm() {
+	let summ = 1 + 2;
+	console.log(summ);
+};
+getSumm();
+
+
+let showMessage = function () {
+	console.log('Привет!');
+};
+showMessage();
+
+
+'use strict'
+if (2 > 1) {
+	function getSumm() {
+		let summ = 1 + 2;
+		console.log(summ);
+	};
+}
+getSumm();
+
+let getSumm;
+
+if (2 > 1) {
+	getSumm = function () {
+		let summ = 1 + 2;
+		console.log(summ);
+	};
+}
+getSumm();
+
+
+let getMessage = (text, name) => text + ', ' + name + '!';
+console.log(getMessage('Привет', 'Вася'));
+
+
+let getMessage = (text, name) => {
+	let message = text + ', ' + name + '!';
+	return message;
+};
+console.log(getMessage('Привет', 'Вася'));
+
+
+function showMessage(text, name) {
+	console.log(`${text}, ${name}!`);
+	setTimeout(showMessage, 500, text, name);
+}
+
+setTimeout(showMessage, 500, 'Привет', 'Вася');
+
+let result = 0;
+function showNumber(num) {
+	result += num;
+	console.log(result);
+	if (result === 5) {
+		clearInterval(timeId);
+	}
+}
+let timeId = setInterval(showNumber, 1000, 1);
+
+
+setTimeout(function () {
+	console.log('Привет!');
+}, 1000);
+
+setTimeout(() => {
+	console.log('Привет!');
+}, 1000);
+
+
+function showMessageOne() {
+	console.log("А я первый!");
+}
+function showMessageTwo() {
+	console.log("А я второй(");
+}
+
+setTimeout(showMessageTwo, 0);
+
+showMessageOne();
+
+
+function createMassage(text, name) {
+	return `${text}, ${name}!`;
+}
+
+function showMassage(message) {
+	console.log(message);
+}
+
+function initMessage(text, name) {
+	showMassage(createMassage(text, name));
+}
+
+initMessage('Привет', 'Фрилансер');
