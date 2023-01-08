f76e24fc404ce7f71e3ec9255668b1dd33017a16
Dmytro Komarov <dmitry.komarov90@gmail.com>
2023-01-08 17:39:10 +0200

    JS events

--- script.js
+++ script.js
@@ -1,150 +1,362 @@
-const mainElement = document.documentElement;
-const mainElementWidth = mainElement.clientWidth;
-const mainElementHeight = mainElement.clientHeight;
 
-console.log(mainElementWidth);
-console.log(mainElementHeight);
+button.onclick = function () {
+	console.log('Клик!');
+}
 
-
-const windowWidth = window.innerWidth;
-const windowHeight = window.innerHeight;
-
-console.log(windowWidth);
-console.log(windowHeight);
+function showConsole() {
+	console.log('Клик!');
+}
+button.onclick = showConsole;
 
 
-let scrollWidth = Math.max(
-document.body.scrollWidth, document.documentElement.scrollWidth,
-document.body.offsetWidth, document.documentElement.offsetWidth,
-document.body.clientWidth, document.documentElement.clientWidth
-);
-let scrollHeight = Math.max(
-document.body.scrollHeight, document.documentElement.scrollHeight,
-document.body.offsetHeight, document.documentElement.offsetHeight,
-document.body.clientHeight, document.documentElement.clientHeight
-);
-console.log(scrollWidth);
-console.log(scrollHeight);
+const button = document.querySelector('.button');
 
-
-const windowScrollTop = window.pageYOffset;
-const windowScrollLeft = window.pageXOffset;
-
-console.log(windowScrollTop);
-console.log(windowScrollLeft);
-
-
-function setScrollBy() {
-window.scrollBy(0, 50);
-const windowScrollTop = window.pageYOffset;
-console.log(windowScrollTop);
+button.onclick = function () {
+	console.log('Клик!');
+}
+button.onclick = function () {
+	console.log('Клак!');
 }
 
 
-function setScrollTo() {
-	window.scrollTo(0, 50);
-	const windowScrollTop = window.pageYOffset;
-	console.log(windowScrollTop);
+const button = document.querySelector('.button');
+
+button.addEventListener("click", function (e) {
+	console.log('Клик!');
+});
+button.addEventListener("click", function (e) {
+	console.log('Клак!');
+});
+
+
+const button = document.querySelector('.button');
+
+function showConsole() {
+	console.log('Клик!');
+	button.removeEventListener("click", showConsole);
 }
 
-function setScrollToOptions() {
-	window.scrollTo({
-		top: 500,
-		left: 0,
-		behavior: "smooth"
-	});
+button.addEventListener("click", showConsole);
+
+
+const button = document.querySelector('.button');
+
+function showConsole() {
+	console.log('Клик!');
+}
+
+button.addEventListener("click", showConsole, options);
+
+
+const button = document.querySelector('.button');
+
+function showConsole(event) {
+
+}
+
+button.addEventListener("click", showConsole);
+
+
+block.addEventListener("click", function (event) {
+	console.log('Клик на Блок!');
+});
+blockInner.addEventListener("click", function (event) {
+	console.log('Клик на Блок второго уровня!');
+}, { "capture": false });
+blockInnerInner.addEventListener("click", function (event) {
+	console.log('Клик на Блок третьего уровня!');
+});
+
+
+const button = document.querySelectorAll('.button');
+
+function showConsole() {
+	console.log('Ура!');
+}
+button.forEach(buttonItem => {
+	buttonItem.addEventListener("click", showConsole);
+});
+
+
+const lesson = document.querySelector('.lesson');
+
+function showConsole() {
+	console.log('Ура!');
+}
+lesson.addEventListener("click", function (event) {
+	if (event.target.closest('.button')) {
+		showConsole();
+	}
+});
+
+
+const menuBody = document.querySelector('.menu');
+
+document.addEventListener("click", menu);
+
+function menu(event) {
+	if (event.target.closest('.menu__button')) {
+		menuBody.classList.toggle('_active');
+	}
+	if (!event.target.closest('.menu')) {
+		menuBody.classList.remove('_active');
+	}
 }
 
 
-function setScrollIntoView(top) {
-	const lessonSelected = document.querySelector('.lesson__selected');
-	lessonSelected.scrollIntoView(top);
+link.addEventListener("click", function (event) {
+	console.log('Наши действия');
+	event.preventDefault();
+});
+*/
+/*
+link.onclick = function () {
+	console.log('Наши действия');
+	return false;
 }
 
-function setScrollIntoViewOptions(top) {
-	const lessonSelected = document.querySelector('.lesson__selected');
-	lessonSelected.scrollIntoView({
-		block: "center",
-		inline: "nearest",
-		behavior: "smooth"
-	});
+const link = document.querySelector('.link');
+
+link.addEventListener("click", function (event) {
+	console.log('Наши действия');
+	// отменить действие браузера (переход по ссылке)
+	event.preventDefault();
+}, { "passive": true });
+
+
+
+const button = document.querySelector('.button');
+
+button.addEventListener("mousedown", function (event) {
+	console.log(`Нажата кнопка ${event.which}`);
+});
+
+button.addEventListener("click", function (event) {
+	console.log('Нажата основная кнопка мыши');
+});
+
+button.addEventListener("contextmenu", function (event) {
+	console.log('Вызвано контекстное меню (не основная кнопка мыши)');
+});
+
+event.which = 1 - Нажата основная кнопка мыши (обычно левая)
+event.which = 2 - Нажата средняя кнопка мыши (колесо)
+event.which = 3 - Нажата не основная кнопка мыши (обычно правая)
+
+
+const blockForMouse = document.querySelector('.block-for-mouse');
+
+blockForMouse.addEventListener("mousemove", function (event) {
+	blockForMouse.innerHTML =
+		`clientX - ${event.clientX} <br> clientY - ${event.clientY}`;
+});
+
+
+
+const blockForMouse = document.querySelector('.block-for-mouse');
+
+blockForMouse.addEventListener("mouseover", function (event) {
+	console.log(event.target);
+	console.log(event.relatedTarget);
+});
+blockForMouse.addEventListener("mouseout", function (event) {
+	console.log(event.target);
+	console.log(event.relatedTarget);
+});
+*/
+
+/*
+const blockForMouse = document.querySelector('.block-for-mouse');
+
+blockForMouse.addEventListener("mouseover", function (event) {
+	console.log(`Курсор над элементом`);
+});
+blockForMouse.addEventListener("mouseout", function (event) {
+	console.log(`Курсор уходит с элемента`);
+});
+
+
+const blockForMouse = document.querySelector('.block-for-mouse');
+
+blockForMouse.addEventListener("mouseenter", function (event) {
+	console.log(`Курсор над элементом`);
+});
+blockForMouse.addEventListener("mouseleave", function (event) {
+	console.log(`Курсор уходит с элемента`);
+});
+
+
+const blockForMouse = document.querySelector('.block-for-mouse');
+blockForMouse.addEventListener("mouseover", function (event) {
+	let target = event.target.closest('span');
+	if (!target) return;
+	target.style.cssText = `background-color: #77608d;`;
+});
+blockForMouse.addEventListener("mouseout", function (event) {
+	let target = event.target.closest('span');
+	if (!target) return;
+	target.style.cssText = ``;
+});
+
+
+const gragField = document.querySelector('.drag-field');
+const gragItem = document.querySelector('.drag-field__item');
+
+gragItem.addEventListener("mousedown", function (event) {
+
+	let coordsItemX = event.clientX - gragItem.getBoundingClientRect().left;
+	let coordsItemY = event.clientY - gragItem.getBoundingClientRect().top;
+
+	let gragItemSizes = {
+		width: gragItem.offsetWidth,
+		height: gragItem.offsetHeight
+	}
+	let gragFieldSizes = {
+		left: gragField.getBoundingClientRect().left + scrollX,
+		top: gragField.getBoundingClientRect().top + scrollY,
+		right: gragField.getBoundingClientRect().left + scrollX + gragField.offsetWidth,
+		bottom: gragField.getBoundingClientRect().top + scrollY + gragField.offsetHeight
+	}
+
+	gragItem.style.position = 'absolute';
+	gragItem.style.zIndex = 1000;
+	document.body.append(gragItem);
+
+	moveItem(event.pageX, event.pageY);
+
+	function moveItem(pageX, pageY) {
+		let currentX = pageX - coordsItemX;
+		let currentY = pageY - coordsItemY;
+
+		if (
+			currentX + gragItemSizes.width <= gragFieldSizes.right &&
+			currentX >= gragFieldSizes.left
+		) {
+			gragItem.style.left = `${currentX}px`;
+		} else {
+			if (currentX + gragItemSizes.width > gragFieldSizes.right) {
+				gragItem.style.left = `${gragFieldSizes.right - gragItemSizes.width}px`;
+			}
+			if (currentX < gragFieldSizes.left) {
+				gragItem.style.left = `${gragFieldSizes.left}px`;
+			}
+		}
+		if (
+			currentY + gragItemSizes.height <= gragFieldSizes.bottom &&
+			currentY >= gragFieldSizes.top
+		) {
+			gragItem.style.top = `${currentY}px`;
+		} else {
+			if (currentY + gragItemSizes.height > gragFieldSizes.bottom) {
+				gragItem.style.top = `${gragFieldSizes.bottom - gragItemSizes.height}px`;
+			}
+			if (currentY < gragFieldSizes.top) {
+				gragItem.style.top = `${gragFieldSizes.top}px`;
+			}
+		}
+	}
+
+	let currentDroppable = null;
+
+	function onDragItem(event) {
+		moveItem(event.pageX, event.pageY);
+
+		gragItem.hidden = true;
+		let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+		gragItem.hidden = false;
+
+		if (!elemBelow) return;
+		let droppableBelow = elemBelow.closest('.drag-field__point');
+
+		if (currentDroppable !== droppableBelow) {
+			if (currentDroppable) {
+				currentDroppable.classList.remove('_active');
+				gragItem.classList.remove('_active');
+			}
+			currentDroppable = droppableBelow;
+			if (currentDroppable) {
+				currentDroppable.classList.add('_active');
+				gragItem.classList.add('_active');
+			}
+		}
+	}
+	document.addEventListener('mousemove', onDragItem);
+
+	document.addEventListener("mouseup", function (event) {
+		document.removeEventListener('mousemove', onDragItem);
+	}, { "once": true });
+});
+gragItem.addEventListener("dragstart", function (event) {
+	event.preventDefault();
+});
+
+
+document.addEventListener("keydown", function (event) {
+	console.log(`Нажата клавиша ${event.code} (${event.key})`);
+});
+document.addEventListener("keyup", function (event) {
+	console.log(`Отжата клавиша ${event.code} (${event.key})`);
+});
+
+
+document.addEventListener('keydown', function (event) {
+	if (event.code == 'KeyZ' && (event.ctrlKey || event.metaKey)) {
+		console.log('Отмена действия!');
+	}
+});
+
+
+document.addEventListener("keydown", function (event) {
+	console.log(`Нажата клавиша ${event.code} (${event.key})`);
+	console.log(event.repeat);
+});
+
+
+const txtItem = document.querySelector('.textarea__item');
+const txtItemLimit = txtItem.getAttribute('maxlength');
+const txtCounter = document.querySelector('.textarea__counter span');
+txtCounter.innerHTML = txtItemLimit;
+
+txtItem.addEventListener("keyup", txtSetCounter);
+txtItem.addEventListener("keydown", function (event) {
+	if (event.repeat) txtSetCounter();
+});
+
+function txtSetCounter() {
+	const txtCounterResult = txtItemLimit - txtItem.value.length;
+	txtCounter.innerHTML = txtCounterResult;
 }
 
 
-function setEnableDisableScroll() {
-	document.body.classList.toggle('scroll-lock');
+document.addEventListener('keyup', function (event) {
+	if (event.code === 'Escape') {
+		menuBody.classList.remove('_active');
+	}
+});
+
+
+window.addEventListener('scroll', function (event) {
+	console.log(`${scrollY}px`);
+});
+
+
+function readyDom() {
+	const image = document.querySelector('.image');
+	console.log(document.readyState);
+	console.log('DOM загружен!');
+	console.log(image.offsetWidth);
+}
+function readyLoad() {
+	console.log(document.readyState);
+	const image = document.querySelector('.image');
+	console.log('Страница загружена!');
+	console.log(image.offsetWidth);
 }
 
 
-const elementOffsetWidth = block.offsetWidth;
-const elementOffsetHeight = block.offsetHeight;
+window.addEventListener("beforeunload", beforeUnLoad);
 
-console.log(elementOffsetWidth);
-console.log(elementOffsetHeight);
-
-
-const elementClientTop = block.clientTop;
-const elementClientLeft = block.clientLeft;
-
-console.log(elementClientTop);
-console.log(elementClientLeft);
-
-
-const elementClientWidth = block.clientWidth;
-const elementClientHeight = block.clientHeight;
-
-console.log(elementClientWidth);
-console.log(elementClientHeight);
-
-
-const elementScrollWidth = block.scrollWidth;
-const elementScrollHeight = block.scrollHeight;
-
-console.log(elementScrollWidth);
-console.log(elementScrollHeight);
-
-
-block.scrollTop = 150;
-
-const elementScrollLeft = block.scrollLeft;
-const elementScrollTop = block.scrollTop;
-
-console.log(elementScrollLeft);
-console.log(elementScrollTop);
-
-
-function setElementScrollBy() {
-	block.scrollBy({
-		top: 20,
-		left: 0,
-		behavior: "smooth"
-	})
+function beforeUnLoad(event) {
+	event.preventDefault();
+	event.returnValue = '';
 }
-
-
-
-const item = document.querySelector('.lesson__item');
-
-const getItemCoords = item.getBoundingClientRect();
-
-console.log(getItemCoords);
-
-const getItemLeftCoord = item.getBoundingClientRect().left;
-
-console.log(getItemLeftCoord);
-
-
-const item = document.querySelector('.lesson__item');
-
-const getItemTopCoord = item.getBoundingClientRect().top;
-
-const getItemTopDocumentCoord = getItemTopCoord + window.pageYOffset;
-
-console.log(getItemTopCoord);
-console.log(getItemTopDocumentCoord);
-
-
-document.elementFromPoint(x, y);
-
-const elem = document.elementFromPoint(100, 100);
-console.log(elem);
